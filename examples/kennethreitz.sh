b831b47bf4f7ef8bfc29122e8bcf8eeee56504c9
Hugh Brown <hughdbrown@yahoo.com>
2024-08-03 20:32:40 -0600

    Add Kenneth Reitz

---
+++ examples/kennethreitz.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+mkdir -p ~/workspace/kennethreitz
+cd ~/workspace/kennethreitz
+
+# page_no = 1
+# https://api.github.com/users/kennethreitz/repos?page=1
+# Skipping 12factor: forked
+git clone https://github.com/kennethreitz/awesome-cdn
+# Skipping background: forked
+git clone https://github.com/kennethreitz/bake
+git clone https://github.com/kennethreitz/bob-builder
+# Skipping bob-builder-1: forked
+# Skipping boost-website: forked
+git clone https://github.com/kennethreitz/bruce-operator
+git clone https://github.com/kennethreitz/changelogdash
+git clone https://github.com/kennethreitz/clint
+git clone https://github.com/kennethreitz/coinbin.org
+# Skipping conda-buildpack: forked
+git clone https://github.com/kennethreitz/context
+# Skipping cppalliance.github.io: forked
+git clone https://github.com/kennethreitz/crayons
+git clone https://github.com/kennethreitz/curated-nodes
+git clone https://github.com/kennethreitz/dive-into-python3
+# Skipping dj-database-url: forked
+# Skipping django-heroku: forked
+# Skipping django-postgrespool: forked
+# Skipping dream-python: forked
+git clone https://github.com/kennethreitz/empartations
+git clone https://github.com/kennethreitz/env
+# Skipping flask-common: forked
+# Skipping flask-googlefed: forked
+# Skipping flask-heroku: forked
+# Skipping flask-sockets: forked
+# Skipping flask-sslify: forked
+git clone https://github.com/kennethreitz/ghsync
+git clone https://github.com/kennethreitz/gistapi.py
+# page_no = 2
+# https://api.github.com/users/kennethreitz/repos?page=2
+# Skipping github-reflog: forked
+# Skipping herald: forked
+# Skipping heroku-buildpack-python: forked
+# Skipping httpbin: forked
+git clone https://github.com/kennethreitz/infinite-state-sample-pack
+# Skipping instructor: forked
+git clone https://github.com/kennethreitz/kennethreitz
+git clone https://github.com/kennethreitz/kr-sphinx-themes
+# Skipping langchain: forked
+git clone https://github.com/kennethreitz/maya
+# Skipping neon-api-python: forked
+git clone https://github.com/kennethreitz/notebooks
+git clone https://github.com/kennethreitz/open-icons
+# Skipping open_777: forked
+# Skipping osx-gcc-installer: forked
+git clone https://github.com/kennethreitz/pep8.org
+# Skipping pip-pop: forked
+# Skipping pipenv: forked
+# Skipping pipenvlib: forked
+# Skipping pydantic: forked
+git clone https://github.com/kennethreitz/pytheory
+git clone https://github.com/kennethreitz/python-github3
+# Skipping python-guide: forked
+git clone https://github.com/kennethreitz/records
+# Skipping replit-py: forked
+# Skipping requests: forked
+# Skipping requests-html: forked
+git clone https://github.com/kennethreitz/requests3
+git clone https://github.com/kennethreitz/resources
+git clone https://github.com/kennethreitz/responder
+# page_no = 3
+# https://api.github.com/users/kennethreitz/repos?page=3
+# Skipping setup.py: forked
+# Skipping static_string: forked
+git clone https://github.com/kennethreitz/super-sphere
+# Skipping tablib: forked
+# Skipping talks: forked
+# Skipping update-pocket: forked
+git clone https://github.com/kennethreitz/wordpress-theme-skeleton
+# page_no = 4
+# https://api.github.com/users/kennethreitz/repos?page=4
