48b968fdd330bf3e7b6ef1af0611352b82e3e9bc
Hugh Brown <hughdbrown@yahoo.com>
2024-12-10 09:18:06 -0700

    Add brookspatton

---
+++ examples/brookspatton.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+mkdir -p ~/workspace/brookspatton
+cd ~/workspace/brookspatton
+
+# page_no = 1
+# https://api.github.com/users/brookspatton/repos?page=1
+# Skipping 2018-dvlpdnvr-talks: forked
+# Skipping 2019-dvlpdnvr-talks: forked
+git clone https://github.com/brookspatton/a127HerokuTest
+git clone https://github.com/brookspatton/add_lambda
+git clone https://github.com/brookspatton/airplane-lander
+# Skipping anathema: forked
+git clone https://github.com/brookspatton/angular-express
+git clone https://github.com/brookspatton/angular-express-postgres-jwt-prototype
+git clone https://github.com/brookspatton/angular-express-server
+git clone https://github.com/brookspatton/angular-playground
+git clone https://github.com/brookspatton/are_files_the_same_in_node
+git clone https://github.com/brookspatton/asteroidsjs
+git clone https://github.com/brookspatton/auth-with-codes-yt
+git clone https://github.com/brookspatton/automate_hello_world_node
+# Skipping barkwire-api: forked
+git clone https://github.com/brookspatton/barkwire-mavo
+git clone https://github.com/brookspatton/barkwire-react-fe
+git clone https://github.com/brookspatton/barkwire_api
+git clone https://github.com/brookspatton/basic-express-api
+git clone https://github.com/brookspatton/bb-pulumi-helpers-js
+git clone https://github.com/brookspatton/bb-pulumi-helpers-ts
+# Skipping bbggez: forked
+git clone https://github.com/brookspatton/bin
+git clone https://github.com/brookspatton/blog
+git clone https://github.com/brookspatton/brcodegg
+git clone https://github.com/brookspatton/brooks-builds-website
+git clone https://github.com/brookspatton/brooks-patton-website
+git clone https://github.com/brookspatton/brooks_builds_notifications
+git clone https://github.com/brookspatton/brookzerker-slack-slash-command
+git clone https://github.com/brookspatton/circle-packing
+# page_no = 2
+# https://api.github.com/users/brookspatton/repos?page=2
+git clone https://github.com/brookspatton/circles
+git clone https://github.com/brookspatton/cli-timer
+git clone https://github.com/brookspatton/code-challenges
+git clone https://github.com/brookspatton/codecrafters-git-rust
+git clone https://github.com/brookspatton/codeship_test
+git clone https://github.com/brookspatton/colored-squares
+# Skipping complete-intro-to-react: forked
+git clone https://github.com/brookspatton/conference_talks
+git clone https://github.com/brookspatton/conquest-game
+git clone https://github.com/brookspatton/cookie-auth-example
+git clone https://github.com/brookspatton/crypto-trading-bot
+git clone https://github.com/brookspatton/crypto-trading-sim
+git clone https://github.com/brookspatton/css-logos
+git clone https://github.com/brookspatton/css-responsive-breakout-20170221
+git clone https://github.com/brookspatton/cupcake-ratings
+# Skipping DefinitelyTyped: forked
+git clone https://github.com/brookspatton/demo-wiki
+git clone https://github.com/brookspatton/denver-startup-week-art-with-code
+git clone https://github.com/brookspatton/deploying-to-firebase
+git clone https://github.com/brookspatton/deploy_full_stack
+git clone https://github.com/brookspatton/development-with-docker-post
+# Skipping discord-manager: forked
+git clone https://github.com/brookspatton/dockerfiles
+git clone https://github.com/brookspatton/docker_hello_world
+git clone https://github.com/brookspatton/docker_sinopia
+# Skipping domShizz: forked
+git clone https://github.com/brookspatton/dotfiles
+git clone https://github.com/brookspatton/eslintrc
+git clone https://github.com/brookspatton/exploring_apollo_vue
+# Skipping express-file-crud: forked
+# page_no = 3
+# https://api.github.com/users/brookspatton/repos?page=3
+git clone https://github.com/brookspatton/express-hbs-example
+git clone https://github.com/brookspatton/fahrenheit-to-celsius-rust
+git clone https://github.com/brookspatton/fire-with-p5js
+git clone https://github.com/brookspatton/fireworks-with-p5
+git clone https://github.com/brookspatton/flappy-bird-clone
+git clone https://github.com/brookspatton/flappy_bird_anathema
+git clone https://github.com/brookspatton/frontend-framework-showdown-places
+git clone https://github.com/brookspatton/full-stack-app
+# Skipping functional-promises: forked
+git clone https://github.com/brookspatton/galvanize-live-streams
+git clone https://github.com/brookspatton/galvanize-secrets-fs
+git clone https://github.com/brookspatton/games_locker
+git clone https://github.com/brookspatton/games_locker_api
+git clone https://github.com/brookspatton/game_template_ggez
+git clone https://github.com/brookspatton/get-latest-twitch-archives
+git clone https://github.com/brookspatton/getsetfactory
+# Skipping ggez: forked
+git clone https://github.com/brookspatton/ghost_server_with_docker
+# Skipping git-chutes-and-ladders: forked
+# Skipping git-graph-drawing: forked
+# Skipping git-rock-paper-scissors: forked
+# Skipping git-with-teams: forked
+git clone https://github.com/brookspatton/github_repository_tracker
+git clone https://github.com/brookspatton/greensock-demo
+git clone https://github.com/brookspatton/grocery_shopping_list
+git clone https://github.com/brookspatton/grokking_deep_learning
+git clone https://github.com/brookspatton/gschool-microservice_memories_example
+# Skipping haikuConflicts: forked
+git clone https://github.com/brookspatton/header_files_in_rust
+git clone https://github.com/brookspatton/hello-world-heroku
+# page_no = 4
+# https://api.github.com/users/brookspatton/repos?page=4
+git clone https://github.com/brookspatton/hello-world-mongo
+git clone https://github.com/brookspatton/hello-world-ruby
+git clone https://github.com/brookspatton/hello_world_html
+git clone https://github.com/brookspatton/hello_world_node
+git clone https://github.com/brookspatton/heroku-test
+git clone https://github.com/brookspatton/heroku_with_postgres
+git clone https://github.com/brookspatton/hof
+git clone https://github.com/brookspatton/html-validator
+git clone https://github.com/brookspatton/html5-meetup-flappy-bird
+# Skipping iac-fundamentals: forked
+git clone https://github.com/brookspatton/ine-project-screeps-mongo
+git clone https://github.com/brookspatton/installed_applications_mac
+git clone https://github.com/brookspatton/install_mailcatcher
+git clone https://github.com/brookspatton/interview_challenge
+git clone https://github.com/brookspatton/iot-stream-marker-button
+git clone https://github.com/brookspatton/javascript-queue-example
+git clone https://github.com/brookspatton/jira-playground
+# Skipping js-algorithms: forked
+git clone https://github.com/brookspatton/js-async-with-generators
+git clone https://github.com/brookspatton/js-test-practice
+git clone https://github.com/brookspatton/jwt-pean_stack
+git clone https://github.com/brookspatton/knex-reference
+git clone https://github.com/brookspatton/knowledge
+git clone https://github.com/brookspatton/knowledge-wiki
+git clone https://github.com/brookspatton/lambdaondigitalocean
+git clone https://github.com/brookspatton/landing-page-that-collects-emails
+git clone https://github.com/brookspatton/ld37
+git clone https://github.com/brookspatton/ld40-twitch-chat-bot-dungeon
+git clone https://github.com/brookspatton/learning-pulumi
+git clone https://github.com/brookspatton/learning-rust
+# page_no = 5
+# https://api.github.com/users/brookspatton/repos?page=5
+git clone https://github.com/brookspatton/learning_neural_networks
+git clone https://github.com/brookspatton/learning_project_todo
+git clone https://github.com/brookspatton/learning_rust_channels
+# Skipping live-chat-manager: forked
+git clone https://github.com/brookspatton/live-stream
+git clone https://github.com/brookspatton/love-calculator
+git clone https://github.com/brookspatton/love-the-pong
+git clone https://github.com/brookspatton/love2d-practice-animations
+git clone https://github.com/brookspatton/love2d-tutorial
+git clone https://github.com/brookspatton/love2d_space_travel
+git clone https://github.com/brookspatton/ludum-dare-38
+git clone https://github.com/brookspatton/ludumdare36
+# Skipping merge-conflict-practice: forked
+git clone https://github.com/brookspatton/meteor-hello_world
+git clone https://github.com/brookspatton/microscope
+git clone https://github.com/brookspatton/mirror_api
+git clone https://github.com/brookspatton/mongo_file_test
+git clone https://github.com/brookspatton/multiplayer_pong_web
+git clone https://github.com/brookspatton/multipleDBTest
+git clone https://github.com/brookspatton/natureofcode
+git clone https://github.com/brookspatton/natureofcode-random-walk
+git clone https://github.com/brookspatton/natureofcode_random_distribution
+git clone https://github.com/brookspatton/nature_of_code_twitch
+git clone https://github.com/brookspatton/networking-with-love2d
+git clone https://github.com/brookspatton/node-authentication-practice-0
+git clone https://github.com/brookspatton/node-meetup
+git clone https://github.com/brookspatton/nodejs-meetup-crud
+git clone https://github.com/brookspatton/nodejs-meetup-docker-compose
+git clone https://github.com/brookspatton/nth_fibonacci_rust
+git clone https://github.com/brookspatton/obs_screens
+# page_no = 6
+# https://api.github.com/users/brookspatton/repos?page=6
+# Skipping ollama: forked
+git clone https://github.com/brookspatton/omdb-demo
+# Skipping pact-5-minute-getting-started-guide: forked
+# Skipping pact-workshop-js: forked
+# Skipping participate_in_code_review: forked
+git clone https://github.com/brookspatton/passive-income-website
+git clone https://github.com/brookspatton/passport-demo
+git clone https://github.com/brookspatton/playground_github_packages
+git clone https://github.com/brookspatton/playground_specs_ggez
+git clone https://github.com/brookspatton/playing-with-angularjs
+git clone https://github.com/brookspatton/playing-with-body-parser
+git clone https://github.com/brookspatton/playing-with-c
+git clone https://github.com/brookspatton/playing-with-git
+git clone https://github.com/brookspatton/playing-with-nested-ng-components
+git clone https://github.com/brookspatton/playing_with_handlebars
+git clone https://github.com/brookspatton/playing_with_hasura
+git clone https://github.com/brookspatton/playing_with_submodules
+git clone https://github.com/brookspatton/playing_with_vue_component_library_client
+git clone https://github.com/brookspatton/power_hour
+# Skipping prep-week2-project1: forked
+git clone https://github.com/brookspatton/prepare_todo_file
+git clone https://github.com/brookspatton/project-ideas
+git clone https://github.com/brookspatton/pwned-checker
+git clone https://github.com/brookspatton/random-walk-lua
+git clone https://github.com/brookspatton/random_walk_ggez
+git clone https://github.com/brookspatton/reddit-clone
+git clone https://github.com/brookspatton/refactoru
+git clone https://github.com/brookspatton/refactoru-gold-rush
+git clone https://github.com/brookspatton/renew-letsencrypt
+# Skipping RESTful-CRUD-Exercise: forked
+# page_no = 7
+# https://api.github.com/users/brookspatton/repos?page=7
+git clone https://github.com/brookspatton/running_platform_builder
+git clone https://github.com/brookspatton/rust-dev-docker
+git clone https://github.com/brookspatton/rusty-chatbot-server
+git clone https://github.com/brookspatton/rusty_pong
+git clone https://github.com/brookspatton/save_to_inbox
+# Skipping screeps-1: forked
+git clone https://github.com/brookspatton/screeps-code
+git clone https://github.com/brookspatton/screeps-docker
+git clone https://github.com/brookspatton/screeps-game
+git clone https://github.com/brookspatton/screeps-private-server
+git clone https://github.com/brookspatton/scripts
+# Skipping sea-orm: forked
+git clone https://github.com/brookspatton/setting_up_computers
+git clone https://github.com/brookspatton/show-pokemon-image
+git clone https://github.com/brookspatton/simple-web-server
+git clone https://github.com/brookspatton/simple_storage
+git clone https://github.com/brookspatton/space-trader-game
+git clone https://github.com/brookspatton/stackit
+git clone https://github.com/brookspatton/stave_off_burnout_by_making_art
+git clone https://github.com/brookspatton/stripe_drill
+git clone https://github.com/brookspatton/sub-module
+git clone https://github.com/brookspatton/sum-api
+git clone https://github.com/brookspatton/super-basic-screeps-starter
+git clone https://github.com/brookspatton/super-simple-game-engine
+# Skipping terminal-mac-cheatsheet: forked
+git clone https://github.com/brookspatton/testbot
+git clone https://github.com/brookspatton/testing-firebase
+git clone https://github.com/brookspatton/testing_main_branchname
+git clone https://github.com/brookspatton/test_blog_book
+# Skipping this-week-in-rust: forked
+# page_no = 8
+# https://api.github.com/users/brookspatton/repos?page=8
+git clone https://github.com/brookspatton/todo-app
+git clone https://github.com/brookspatton/todo-cli
+git clone https://github.com/brookspatton/todo-experiments
+git clone https://github.com/brookspatton/todoapp_heroku_demo
+git clone https://github.com/brookspatton/todo_backlog
+git clone https://github.com/brookspatton/twitch-channel-status
+git clone https://github.com/brookspatton/twitch-crypto
+git clone https://github.com/brookspatton/twitch-crypto-dashboard
+git clone https://github.com/brookspatton/twitch-overlay
+git clone https://github.com/brookspatton/twitch-overlays
+git clone https://github.com/brookspatton/twitch-wager
+# Skipping twitch_api: forked
+git clone https://github.com/brookspatton/twitter-vacuum
+git clone https://github.com/brookspatton/update-all-docker-images
+git clone https://github.com/brookspatton/update-ubuntu
+git clone https://github.com/brookspatton/visualizing-random-numbers-love2d
+git clone https://github.com/brookspatton/vue-todo
+git clone https://github.com/brookspatton/web_component_test
+git clone https://github.com/brookspatton/web_dev_rehabilitation
+# Skipping whats-for-lunch: forked
+git clone https://github.com/brookspatton/what_to_do_today
+git clone https://github.com/brookspatton/wiki-blog
+git clone https://github.com/brookspatton/world-from-nature-of-code
+# page_no = 9
+# https://api.github.com/users/brookspatton/repos?page=9
